5b3bade5572e16258935334d3fb6cb0788e04755
ogmegumi <ogmegumi@gmail.com>
2021-06-11 16:34:10 +0000

    [update]4-6 validate

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'e8816ef57d06b217c365468fafd841966e3b4c4d9089b9464d7e90861bd37de8f40fb1789bfc82cb2ad8e0519e1b2eccbe3d619024a0f2fae6c1aaf506963882'
